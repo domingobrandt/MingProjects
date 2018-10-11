4af360437f8268bac382c47fef09f32d8b6c04af
Domingo Brandt <brandtyet02@gmail.com>
2018-10-11 02:07:18 -0400

    angular animations button states

--- src/app/inventory/inventory.component.ts
+++ src/app/inventory/inventory.component.ts
@@ -1,12 +1,27 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-inventory',
   templateUrl: './inventory.component.html',
-  styleUrls: ['./inventory.component.scss']
+  styleUrls: ['./inventory.component.scss'],
+  animations: [
+    trigger('animacion', [
+        state('inactive', style({
+            transform: 'scale(1)'
+        })),
+        state('active', style({
+            backgroundColor: '#cfd8dc',
+            transform: 'scale(1.1)'
+        })),
+        transition('inactive => active', animate('100ms ease-in')),
+        transition('active => inactive', animate('100ms ease-out'))
+    ])
+]
 })
 export class InventoryComponent implements OnInit {
-
+  estado1 = "inactive";
+  estado2 = "inactive";
   constructor() { }
 
   ngOnInit() {
